d245a9291eb22937b37e7ee4858f67f853eb03cc
upsydedown <chintansenapati@gmail.com>
2024-03-23 23:06:54 +0530

    Initial Commit (Structuring files)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,30 @@
 import React from "react";
 import "./Navbar.css";
+import { Navlink, useMatch, useResolvedPath } from "react-router-dom";
+
+const CustomLink = ({ to, content, ...children }) => {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Navlink to={to} {...children}>
+        {content}
+      </Navlink>
+    </li>
+  );
+};
+
 const Navbar = () => {
   return (
     <>
       <section className="navbarMain">
         <div className="navbody">
-          <nav></nav>
+          <nav>
+            <CustomLink to="/" content="Home" />
+            <CustomLink to="/" content="Home" />
+            <CustomLink to="/" content="Home" />
+            <CustomLink to="/" content="Home" />
+          </nav>
         </div>
       </section>
     </>
