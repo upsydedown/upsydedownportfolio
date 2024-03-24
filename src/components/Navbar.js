e08b41bb0026d2e8c159585f7924c77a28fd8605
upsydedown <chintansenapati@gmail.com>
2024-03-24 08:09:24 +0530

    Updating Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { Navlink, useMatch, useResolvedPath } from "react-router-dom";
 
-const CustomLink = ({ to, content, ...children }) => {
-  const resolvedPath = useResolvedPath(to);
-  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
-  return (
-    <li className={isActive ? "active" : ""}>
-      <Navlink to={to} {...children}>
-        {content}
-      </Navlink>
-    </li>
-  );
-};
+// const CustomLink = ({ to, content, ...children }) => {
+//   const resolvedPath = useResolvedPath(to);
+//   const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+//   return (
+//     <li className={isActive ? "active" : ""}>
+//       <Navlink to={to} {...children}>
+//         {content}
+//       </Navlink>
+//     </li>
+//   );
+// };
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false); // State to track navbar visibility on mobile
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen); // Toggle navbar visibility
+  };
   return (
     <>
       <section className="navbarMain">
-        <div className="navbody">
-          <nav>
-            <CustomLink to="/" content="Home" />
-            <CustomLink to="/" content="Home" />
-            <CustomLink to="/" content="Home" />
-            <CustomLink to="/" content="Home" />
+        <div className={`nav ${isOpen ? "active" : ""}`}>
+          <nav className="navMenu">
+            <li className="navItem">
+              <a href="">Home</a>
+            </li>
+            <li className="navItem">
+              <a href="">About</a>
+            </li>
+            <li className="navItem">
+              <a href="">Projects</a>
+            </li>
+            <li className="navItem">
+              <a href="">Play</a>
+            </li>
+            <li className="navItem">
+              <a href="">Resume</a>
+            </li>
           </nav>
         </div>
       </section>
