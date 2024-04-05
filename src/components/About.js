ac5077163cfdd2ae20d075e3d65b479f3f9c5611
upsydedown <chintansenapati@gmail.com>
2024-04-06 03:12:29 +0530

    About Page

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,30 @@
 import React from "react";
+import "./About.css";
+
 
 const About = () => {
-  return <div>About</div>;
+  return (
+
+
+
+
+    <div className="hexagon-grid">
+
+      <img src="https://images.unsplash.com/photo-1584023878218-888f8caadcae?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" srcset="" />
+      <img src="https://images.unsplash.com/photo-1584023878218-888f8caadcae?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" srcset="" />
+      <img src="https://images.unsplash.com/photo-1584023878218-888f8caadcae?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" srcset="" />
+      <img src="https://images.unsplash.com/photo-1584023878218-888f8caadcae?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" srcset="" />
+      <img src="https://images.unsplash.com/photo-1584023878218-888f8caadcae?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" srcset="" />
+      <img src="https://images.unsplash.com/photo-1584023878218-888f8caadcae?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" srcset="" />
+
+    </div>
+
+
+
+
+
+
+  );
 };
 
 export default About;
