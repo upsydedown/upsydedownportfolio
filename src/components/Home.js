0abfffa117e477c29a45704c84339b0a914fdb12
upsydedown <chintansenapati@gmail.com>
2024-04-05 22:43:08 +0530

    Animation of Homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,14 +8,18 @@ const Home = () => {
       <section className="homeMain">
         <div className="flex homeBody">
           <div className="home__LeftContainer">
-            <img src="" alt="" srcset="" />
+            <img className="home__Image" src="https://images.unsplash.com/photo-1707343844152-6d33a0bb32c3?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" srcset="" />
+            <div className="ring-container">
+              <div className="ring ring-1"></div>
+              <div className="ring ring-2"></div>
+            </div>
           </div>
           <div className="flex home__RightContainer">
-            <div className="home__HeadingTextContainer">
+            <div className="flex home__HeadingTextContainer">
               <h1 className="home__HeaderText">
                 Chintan Senapati
               </h1>
-              <div className="home__CaptionTextContainer">
+              <div className="flex home__CaptionTextContainer">
                 <span className="cap animate__animated animate__fadeInUp animate__delay-3s">
                   UI/UX
                 </span>
