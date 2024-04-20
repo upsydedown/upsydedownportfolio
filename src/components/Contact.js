bbfa4572ddaa20e8cf31d60c179c325ebc427b21
upsydedown <chintansenapati@gmail.com>
2024-04-21 00:13:57 +0530

    Contact Page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,15 +7,32 @@ const Contact = () => {
       <div className="flex contactBody">
         <div className="flex contact__LeftContainer">
           <img src="./images/Contact/tree.png" alt="" srcset="" />
-          <div className="contact__LeftContainer__Icon">
-            <Icon />
+          <div className="flex contact__LeftContainer__Icon">
+            <Icon
+              style="tree_icon"
+              heading=""
+              image="./images/Contact/linkedin.png"
+            />
+            <Icon
+              style="tree_icon"
+              heading=""
+              image="./images/Contact/twitter.png"
+            />
+            <Icon
+              style="tree_icon"
+              heading=""
+              image="./images/Contact/mail.png"
+            />
           </div>
         </div>
         <div className="flex contact__RightContainer">
           <h1></h1>
           <div className="contact__RightContainer__btn">
+            <h1>Send an Owl</h1>
             <img src="./images/Contact/orb.png" alt="" srcset="" />
-            <div className="contact__RightContainer__QuoteBox"></div>
+            <div className="contact__RightContainer__QuoteBox">
+              <p></p>
+            </div>
           </div>
         </div>
       </div>
